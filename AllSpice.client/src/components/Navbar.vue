b0249564900f5fc1e01981d02730d6036a89bf0b
tif <youfoundtiffany@gmail.com>
2023-10-14 15:25:17 -0600

    styles

--- AllSpice.client/src/components/Navbar.vue
+++ AllSpice.client/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg px-3 Lora bg-UltraDrkNutmeg navCorners">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
         <img alt="logo" src="../assets/img/asLogoOnly.svg" height="85" />
@@ -12,13 +12,20 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
+          <router-link :to="{ name: 'About' }" class="btn lighten-30 selectable">
             About
           </router-link>
         </li>
       </ul>
+      <!-- FIXME move to backgroun header image -->
+      <!-- <div class="p-1">
+        <div class="input-group">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </div>
+      </div> -->
       <!-- LOGIN COMPONENT HERE -->
-      <Login />
+      <!-- <Login /> -->
     </div>
   </nav>
 </template>
@@ -34,6 +41,10 @@ export default {
 </script>
 
 <style scoped>
+.navCorners {
+  border-radius: 22px;
+}
+
 a:hover {
   text-decoration: none;
 }
@@ -46,6 +57,7 @@ a:hover {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
+
 }
 
 @media screen and (min-width: 768px) {
--- AllSpice.client/src/pages/HomePage.vue
+++ AllSpice.client/src/pages/HomePage.vue
@@ -1,10 +1,44 @@
 <template>
   <div class="container pt-2">
+    <!-- Banner section -->
     <section class="row">
-      <Navbar />
-
+      <div class="col-12 bannerImg shadow">
+        <!-- Search bar and user icon row -->
+        <div class="row justify-content-end">
+          <div class="col-md-4">
+            <div class="input-group">
+              <input class="form-control py-2 border-right-0 border" type="search" value="search"
+                id="example-search-input">
+              <span class="input-group-append">
+                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+              </span>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <!-- LOGIN COMPONENT HERE -->
+            <div class="user-icon">
+              <Login />
+            </div>
+          </div>
+        </div>
+        <!-- Banner title and paragraphs -->
+        <div class="banner-content text-center pb-2">
+          <h1 class="All-Spice">Your Website Title</h1>
+          <p class="text-MochaSlate">Your paragraph 1</p>
+          <p class="text-MochaSlate">Your paragraph 2</p>
+        </div>
+      </div>
 
     </section>
+
+    <!-- Navbar section -->
+    <div class="container pt-2">
+      <section class="row d-flex justify-content-center" style="margin-top: -40px;"> <!-- Adjust the margin-top value -->
+        <div class="col-6">
+          <Navbar class="shadow" />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -18,7 +52,27 @@ export default {
 
 
 <style scoped lang="scss">
-.navImg {
-  background-image: url();
+.shadow {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  /* Adjust the values as needed */
+}
+
+.bannerImg {
+  background-image: url("src/assets/img/love.jpg");
+  max-height: 250vh;
+  width: 100%;
+  object-fit: cover;
+  background-position: center bottom;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 10px !important;
+}
+
+
+/* Media query for screens below 600px */
+@media screen and (max-width: 599px) {
+  .bannerImg {
+    background-image: url("src/assets/img/spoons.jpg");
+  }
 }
 </style>
